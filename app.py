c8c812405074c06fb152c6cd3fd83b0a40e00a60
Tássio Auad <auadtassio@gmail.com>
2020-02-29 15:00:02 -0300

    feat: Pasta templates  o app.py
    Arquivos html movidos todos para a pasta padrão templates.
    Foi criado o arquivo app.py roteando os caminhos para a renderização dos htmls.

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/sobre")
+def sobre():
+    return render_template("about.html")
+
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+
+@app.route("/blog/detalhes")
+def blogDetalhes():
+    return render_template("single-blog.html")
+
+
+@app.route("/contato")
+def contato():
+    return render_template("contact.html")
+
+
+@app.route("/industria")
+def industria():
+    return render_template("industries.html")
+
+
+@app.route("/trabalho")
+def trabalho():
+    return render_template("work.html")
